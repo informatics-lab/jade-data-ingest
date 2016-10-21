0a02fb8c66d4a90376e01e09f83d4fa2b9d69c33
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2016-10-21 17:09:14 +0100

    tweek

--- ingester.tf
+++ ingester.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "ingester" {
   key_name              = "gateway"
   user_data             = "${data.template_file.bootstrap.rendered}"
 //  iam_instance_profile  = "${aws_iam_role.data_ingester.id}"
-  iam_instance_profile  = "jade-secrets"
+  iam_instance_profile  = "jade-data-ingest"
   tags {
     Name = "jade-data-ingest"
   }
